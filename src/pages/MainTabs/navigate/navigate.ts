83816197ac2afe86718f9549df7f61eb674a1792
arqam-ebani <arqam@ebaniadvertising.in>
2019-04-09 12:10:54 +0530

    Map Loaded

--- src/pages/MainTabs/navigate/navigate.ts
+++ src/pages/MainTabs/navigate/navigate.ts
@@ -32,6 +32,11 @@ export class NavigatePage {
   }
 
   loadMap() {
+    // Environment.setEnv({
+    //   'API_KEY_FOR_BROWSER_RELEASE': '(your api key for `https://`)',
+    //   'API_KEY_FOR_BROWSER_DEBUG': '(your api key for `http://`)'
+    // });
+
     // Create a map after the view is loaded.
     // (platform is already ready in app.component.ts)
     this.map = GoogleMaps.create('map_canvas', {
@@ -53,33 +58,61 @@ export class NavigatePage {
     // Get the location of you
     this.map.getMyLocation()
       .then((location: MyLocation) => {
-        console.log(JSON.stringify(location, null, 2));
+        console.log(JSON.stringify(location.latLng));
 
-        // Move the map camera to the location with animation
-        this.map.animateCamera({
-          target: location.latLng,
-          zoom: 17,
-          tilt: 30
-        })
-          .then(() => {
-            // add a marker
-            let marker: Marker = this.map.addMarkerSync({
-              title: '@ionic-native/google-maps plugin!',
-              snippet: 'This plugin is awesome!',
-              position: location.latLng,
-              animation: GoogleMapsAnimation.BOUNCE
-            });
 
-            // show the infoWindow
-            marker.showInfoWindow();
 
-            // If clicked it, display the alert
-            marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
-              this.showToast('clicked!');
-            });
-          });
-      }); 
+
+
+
+
+
+
+
+
+
+
+        // // Move the map camera to the location with animation
+        // this.map.animateCamera({
+        //   target: location.latLng,
+        //   zoom: 30,
+        //   tilt: 0
+        // }).then(() => {
+        //     // add a marker
+        //     let marker: Marker = this.map.addMarkerSync({
+        //       title: '@ionic-native/google-maps plugin!',
+        //       snippet: 'This plugin is awesome!',
+        //       position: location.latLng,
+        //       animation: GoogleMapsAnimation.BOUNCE
+        //     });
+
+        //     // show the infoWindow
+        //     marker.showInfoWindow();
+
+        //     // If clicked it, display the alert
+        //     marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+        //       this.showToast('clicked!');
+        //     });
+        //   });
+      });
   }
+
+  addMarker() {
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 17.4030548,
+        lng: 78.4201877
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+
+  }
+
 
   showToast(message: string) {
     let toast = this.toastCtrl.create({
